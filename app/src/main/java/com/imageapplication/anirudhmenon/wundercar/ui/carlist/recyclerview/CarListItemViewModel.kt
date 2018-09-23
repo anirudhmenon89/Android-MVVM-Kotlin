60ecc35e4e80167e2c9d0e910916702dfe015c31
Anirudh <anirudh.menon@tlkn.com>
2018-09-23 14:08:22 +0530

    Minor refactoring in CarListItemViewModel

--- app/src/main/java/com/imageapplication/anirudhmenon/wundercar/ui/carlist/recyclerview/CarListItemViewModel.kt
+++ app/src/main/java/com/imageapplication/anirudhmenon/wundercar/ui/carlist/recyclerview/CarListItemViewModel.kt
@@ -4,21 +4,9 @@ import android.databinding.ObservableField
 import android.view.View
 import com.imageapplication.anirudhmenon.wundercar.ui.data.model.api.CarInfo
 
-class CarListItemViewModel {
-    var address: ObservableField<String>
-    var name: ObservableField<String>
-    var carInfo: CarInfo
-    var clickListener: CarListItemClick
-
-    constructor(carInfo: CarInfo, clickListener: CarListItemClick) {
-        this.carInfo = carInfo
-
-        address = ObservableField(carInfo.address)
-        name = ObservableField(carInfo.name)
-
-        this.clickListener = clickListener
-
-    }
+class CarListItemViewModel(carInfo: CarInfo, var clickListener: CarListItemClick) {
+    var address: ObservableField<String> = ObservableField(carInfo.address)
+    var name: ObservableField<String> = ObservableField(carInfo.name)
 
     fun onItemClick(view: View, viewModel: CarListItemViewModel) {
         clickListener.onItemClick(view, viewModel)
